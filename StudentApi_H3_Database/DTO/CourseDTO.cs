e992d6d2023521cccfcc91c8bdda36605e8b459a
Bjoern <bjoe6880@elev.techcollege.dk>
2023-10-30 10:48:10 +0100

    Added CourseDTO and StudentCourseDTO and linked them

---
+++ StudentApi_H3_Database/DTO/CourseDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace StudentApi_H3_Database.DTO
+{
+    public class SaveCourseDTO
+    {
+        public string CourseName { get; set; }
+    }
+
+    public class UpdateCourseDTO : SaveCourseDTO
+    {
+        public int CourseId { get; set; }
+    }
+
+    public class CourseDTO : UpdateCourseDTO
+    {
+        //Referer til StudentCourses
+        public ICollection<StudentCourseDTOMinusCourse> StudentCourse { get; set; }
+    }
+
+    public class CourseDTOMinusRelation : UpdateCourseDTO
+    {
+
+    }
+}
---
+++ StudentApi_H3_Database/DTO/StudentCourseDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace StudentApi_H3_Database.DTO
+{
+    public class SaveAndUpdateStudentCourseDTO
+    {
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+    }
+
+    public class StudentCourseDTO : SaveAndUpdateStudentCourseDTO
+    {
+        public StudentDTOMinusRelation Student {  get; set; }   
+        public CourseDTOMinusRelation Course {  get; set; }   
+    }
+
+    public class StudentCourseDTOMinusCourse : SaveAndUpdateStudentCourseDTO
+    {
+        public StudentDTOMinusRelation Student { get; set; }
+    }
+
+    public class StudentCourseDTOMinusStudent : SaveAndUpdateStudentCourseDTO
+    {
+        public CourseDTOMinusRelation Course { get; set; }  
+    }
+}
