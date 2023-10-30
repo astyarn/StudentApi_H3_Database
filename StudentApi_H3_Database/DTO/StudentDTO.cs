e401acc621e1b46d4bf8695f37b034c519214ef6
Bjoern <bjoe6880@elev.techcollege.dk>
2023-10-30 10:17:51 +0100

    Created StudentDTO and TeamDTO. Connected TeamDTO to StudentDTO and vise versa

---
+++ StudentApi_H3_Database/DTO/StudentDTO.cs
@@ -0,0 +1,26 @@
+﻿namespace StudentApi_H3_Database.DTO
+{
+    public class SaveStudentDTO
+    {
+        public string StudentName { get; set; }
+        public int StudentAge { get; set; }
+        public int TeamId { get; set; }
+    }
+
+    public class UpdateStudentDTO : SaveStudentDTO
+    {
+        public int StudentId { get; set; }
+    }
+
+    public class StudentDTO : UpdateStudentDTO 
+    {
+        public TeamDTOMinusRelation? Team { get; set; }
+
+        //Referer Alle courses
+    }
+
+    public class StudentDTOMinusRelation : UpdateStudentDTO
+    {
+        
+    }
+}
---
+++ StudentApi_H3_Database/DTO/TeamDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace StudentApi_H3_Database.DTO
+{
+    public class SaveTeamDTO
+    {
+        public string TeamName { get; set; }
+        public int TeamDescribtion { get; set; }
+    }
+
+    public class UpdateTeamDTO : SaveTeamDTO
+    {
+        public int TeamId { get; set; }
+    }
+
+    public class TeamDTO : UpdateTeamDTO
+    {
+        public ICollection<StudentDTOMinusRelation> Students { get; set; }  
+    }
+
+    public class TeamDTOMinusRelation : UpdateTeamDTO
+    {
+
+    }
+}
